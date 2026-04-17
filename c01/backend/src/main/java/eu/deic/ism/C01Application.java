ac37efac8c42ba021a086122833e1fde253e45a2
Tudor Gheorghiu <tudi.gheorghiu@gmail.com>
2026-04-17 12:49:29 +0300

    feat: scaffold Spring Boot project in C01

---
+++ c01/backend/src/main/java/eu/deic/ism/C01Application.java
@@ -0,0 +1,13 @@
+package eu.deic.ism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C01Application.class, args);
+	}
+
+}
